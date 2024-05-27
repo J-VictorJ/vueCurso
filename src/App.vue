ad01a6a230d61586c91754972d6af060cf4504a0
João Victor <padrinhoreserva@gmail.com>
2024-05-27 17:51:19 -0300

    v-slot

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,15 @@
 
 <template>
-  <h3>
-    Slot in vue
-  </h3>
-  <p>Card-like from foods array</p>
-  <div id="wrapper">
-      <slot-comp v-for="x in foods">
-        <img v-bind:src="x.url">
-        <h4>{{ x.name }}</h4>
-        <p>{{ x.desc }}</p>
-      </slot-comp>
-    </div>
-    <footer>
-      <slot-comp>
-        <h4>Footer</h4>
-      </slot-comp>
-    </footer>
+ <slot-comp v-slot:default>
+  Hello
+ </slot-comp>
 </template>
 
 <script>
-export default{
-  data(){
-    return{
-      foods: [
-      { name: 'Apple', desc: 'Apples are a type of fruit that grow on trees.', url: 'img/img_apple.svg'},
-      { name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', url: 'img/img_pizza.svg'},
-      { name: 'Cake', desc: 'Cake is something sweet that tates good but is not consodered healthy.', url: 'img/img_cake.svg'},
-      { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', url: 'img/img_rice.svg'},
-    ]
-    }
-  }
-}
- 
 </script>
 
 <style>
-#wrapper {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-#wrapper > div {
-  background-color: lightgreen;
-}
-#wrapper img {
-  display: block; 
-  margin: 20% auto 0; 
-  width: 60%;
-}
-footer > div {
-  background-color: lightpink;
-}
-h3, h4{
-  text-align: center;
+#app{
+  width: 300px;
 }
 </style>
--- src/components/SlotComp.vue
+++ src/components/SlotComp.vue
@@ -2,21 +2,22 @@
     <div>
         <slot></slot>
     </div>
+    <div>
+        <slot name="bottomSlot"></slot>
+    </div>
 </template>
 <script>
+// slot shorthand = #
 </script>
+
 <style scoped>
 div {
-    background-color: lightgreen;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    flex-basis: 150px;
-    border-radius: 10px;
-    border: solid black 2px;
+    height: 30px;
+    width: 50%;
+    border: dotted black 1px;
     margin: 10px;
-    padding: 20px 10px 0 10px;
-}
-div:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    padding: 10px;
+    background-color: lightgreen;
+    font-weight: bold;
 }
 </style>
