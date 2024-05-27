2ee1c6a5c9f3b6c6d37e7161cc11cad2b194a79b
João Victor <padrinhoreserva@gmail.com>
2024-05-27 17:17:53 -0300

    Slots as Cards

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,41 @@
   <h3>
     Slot in vue
   </h3>
-  <p>Hello World sent as a content to the slot tag</p>
-  <slot-comp>Hello World</slot-comp>
+  <p>Card-like from foods array</p>
+  <div id="wrapper">
+      <slot-comp v-for="x in foods">
+        <img v-bind:src="x.url">
+        <h4>{{ x.name }}</h4>
+        <p>{{ x.desc }}</p>
+      </slot-comp>
+      
+    </div>
 </template>
 
 <script>
-export default{}
+export default{
+  data(){
+    return{
+      foods: [
+      { name: 'Apple', desc: 'Apples are a type of fruit that grow on trees.', url: 'img/img_apple.svg'},
+      { name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', url: 'img/img_pizza.svg'},
+      { name: 'Cake', desc: 'Cake is something sweet that tates good but is not consodered healthy.', url: 'img/img_cake.svg'},
+      { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', url: 'img/img_rice.svg'},
+    ]
+    }
+  }
+}
  
 </script>
 
 <style>
-p{
-  width: 200px;
-}
-#app div {
-  border: dashed black 1px;
-  margin: 10px;
-  padding: 10px;
-  display: inline-block;
+#wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+#wrapper img {
+  display: block; 
+  margin: auto; 
+  width: 60%;
 }
 </style>
