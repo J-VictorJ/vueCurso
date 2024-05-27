6a7e2d15fc73ab8849e4d22d40b89007cba410c9
João Victor <padrinhoreserva@gmail.com>
2024-05-27 18:49:31 -0300

    Scoped Slots

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
-
 <template>
- <slot-comp v-slot:default>
-  Hello
- </slot-comp>
+    <h1>Scoped Slots</h1>
+  <slot-comp v-slot="food">
+      <hr>
+      <h2>{{ food.foodName }}<img :src=food.foodUrl></h2>
+      <p class="greenP">{{ food.foodDesc }}</p>
+  </slot-comp>
 </template>
 
 <script>
 </script>
 
 <style>
-#app{
-  width: 300px;
+ #app {
+    width: 300px;
+  }
+h2, .greenP {
+  background-color: lightgreen;
+  padding: 10px;
+  margin: 0;
+}
+img {
+  float: right;
+  height: 70px;
+  margin-left: 10px;
 }
 </style>
--- src/components/SlotComp.vue
+++ src/components/SlotComp.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-        <slot></slot>
-    </div>
-    <div>
-        <slot name="bottomSlot"></slot>
+        <slot
+        v-for="x in foods"
+        :key="x.name"
+        :foodName="x.name"
+        :foodDesc="x.desc"
+        :foodUrl="x.url"></slot>
     </div>
 </template>
 <script>
-// slot shorthand = #
+export default{
+    data(){
+        return{
+            foods: [
+                { name: 'Apple', desc: 'Apples are a type of fruit that grow on trees.', url: 'img/img_apple.svg' },
+                { name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', url: 'img/img_pizza.svg' },
+                { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', url: 'img/img_rice.svg' },
+                { name: 'Cake', desc: 'Cake is something sweet that tates good but is not consodered healthy.', url: 'img/img_cake.svg' }
+            ]
+        }
+    }
+}
 </script>
 
 <style scoped>
